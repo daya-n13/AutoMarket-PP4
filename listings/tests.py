2b158ac222f99f17f66163917cdd4179f6c5a479
daya-n13 <dnashkova@google.com>
2025-04-22 12:18:35 +0000

    update test.py

--- listings/tests.py
+++ listings/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+class HomePageTests(TestCase):
+    def test_home_page(self):
+        ''''
+        tests if home page can be accessed
+        '''
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "listings/index.html")
